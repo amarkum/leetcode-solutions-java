8fcf268aab0a39d290a3e113c8374badc3bccc19
Kumar <amarkumar@kpmg.com>
2020-05-02 00:08:48 +0530

    Added Code for Palindrome of LinkedList

---
+++ src/main/java/com/leetcode/solutions/linkedlist/PalindromeLinkedList.java
@@ -0,0 +1,34 @@
+package com.leetcode.solutions.linkedlist;
+
+class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = head;
+        slow = reversed(slow);
+        while (fast != null && slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    public ListNode reversed(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
