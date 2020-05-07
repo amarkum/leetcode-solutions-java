1533098a33cf789fc4d8b1f21083e56f6b351f41
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-07 23:21:16 +0530

    BST Range Sum

---
+++ src/main/java/com/leetcode/solutions/tree/BSTRangeSum.java
@@ -0,0 +1,8 @@
+package com.leetcode.solutions.tree;
+
+/**
+ * Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+ * The binary search tree is guaranteed to have unique values.
+ */
+public class BSTRangeSum {
+}
