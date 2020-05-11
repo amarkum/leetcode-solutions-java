726d729604a3f64dfbd101c1299b5644e7b54fe6
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-11 15:38:13 +0530

    Added BST Range Sum

--- src/main/java/com/leetcode/solutions/tree/BSTRangeSum.java
+++ src/main/java/com/leetcode/solutions/tree/BSTRangeSum.java
@@ -4,5 +4,28 @@ package com.leetcode.solutions.tree;
  * Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
  * The binary search tree is guaranteed to have unique values.
  */
-public class BSTRangeSum {
+class BSTRangeSum {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        parseNode(root, L, R);
+        return sum;
+    }
+
+    public void parseNode(TreeNode node, int L, int R) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.val >= L && node.val <= R) {
+                sum += node.val;
+        }
+
+        if(node.val > L) {
+            parseNode(node.left, L, R);
+        }
+
+        if(node.val < R) {
+            parseNode(node.right, L, R);
+        }
+    }
 }
