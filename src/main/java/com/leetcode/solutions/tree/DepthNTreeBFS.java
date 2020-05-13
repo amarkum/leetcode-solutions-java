11a8f0fe4997a9591edc280a7e4498e10b862995
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-13 13:30:41 +0530

    Added code for Breadth First Search, Finding Max Depth - N Tree

---
+++ src/main/java/com/leetcode/solutions/tree/DepthNTreeBFS.java
@@ -0,0 +1,43 @@
+package com.leetcode.solutions.tree;
+
+import java.util.LinkedList;
+
+public class DepthNTreeBFS {
+
+    public int maxDepth(Node root) {
+        LinkedList<Node> stack = new LinkedList<Node>();
+        int depth = 0;
+
+        // if root is null, return 0 as depth
+        if (root == null) {
+            return depth;
+        }
+
+        // add root node to stack
+        stack.offer(root);
+
+        while (!stack.isEmpty()) {
+
+            // calculate size of the current level
+            int size = stack.size();
+
+            // iterate every node in that level
+            for (int i = 0; i < size; i++) {
+
+                // Pop up all the node by the stack size, set previously
+                Node node = stack.pop();
+
+                // add all the children at that level
+                for (Node child : node.children) {
+                    stack.offer(child);
+                }
+            }
+
+            // increase the depth, when all the node at the level is popped,and children added
+            depth++;
+        }
+
+        // return the depth
+        return depth;
+    }
+}
