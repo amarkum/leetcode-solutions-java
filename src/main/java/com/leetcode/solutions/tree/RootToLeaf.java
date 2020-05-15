bc5c4c5f2911dc03a772e6387f357826e3081117
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-16 01:30:57 +0530

    Added code for Binary Tree Paths

--- src/main/java/com/leetcode/solutions/tree/RootToLeaf.java
+++ src/main/java/com/leetcode/solutions/tree/RootToLeaf.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 257. Binary Tree Paths - https://leetcode.com/problems/binary-tree-paths/
+ *
+ * Given a binary tree, return all root-to-leaf paths.
+ */
 class RootToLeaf {
     LinkedList <Integer> stack = new LinkedList <>();
     List <String> values = new ArrayList <>();
@@ -21,19 +26,24 @@ class RootToLeaf {
             return;
         }
 
+        // Add the value to the stack
         stack.offer(node.val);
 
+        // If we encounter leaf node, we have come across one of the path
         if (node.left == null && node.right == null) {
             String rootToLeaf = "";
             for (int number: stack) {
                 rootToLeaf += number + "->";
             }
+
+            // remove the arrow at the last
             values.add(rootToLeaf.substring(0, rootToLeaf.length() - 2));
         }
 
         preOrderDFS(node.left);
         preOrderDFS(node.right);
 
+        // poll the last element from the stack.
         stack.pollLast();
     }
 }
