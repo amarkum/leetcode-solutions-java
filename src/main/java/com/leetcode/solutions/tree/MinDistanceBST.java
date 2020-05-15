039dc577b532e7974f175ebb379c6cc16757754f
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-16 02:31:01 +0530

    Added code for Minimum Distance Between BST Nodes

---
+++ src/main/java/com/leetcode/solutions/tree/MinDistanceBST.java
@@ -0,0 +1,35 @@
+package com.leetcode.solutions.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 783. Minimum Distance Between BST Nodes - https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+ *
+ * Given a Binary Search Tree (BST) with the root node root,
+ * return the minimum difference between the values of any two different nodes in the tree.
+ */
+class Solution {
+    int minimumDifference = Integer.MAX_VALUE;
+    List<Integer> list = new ArrayList<>();
+
+    public int minDiffInBST(TreeNode root) {
+        inOrderDFS(root);
+
+        // Re-assign the minimum difference if any
+        for(int i=1; i<list.size(); i++){
+            minimumDifference = Math.min(list.get(i)-list.get(i-1), minimumDifference);
+        }
+        return minimumDifference;
+    }
+
+    // Do a In Order Traversal
+    public void inOrderDFS(TreeNode node){
+        if(node==null){
+            return;
+        }
+        inOrderDFS(node.left);
+        list.add(node.val);
+        inOrderDFS(node.right);
+    }
+}
