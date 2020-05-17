cfcac84aeabd6d8a18953dcd876dee6d88626c4d
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-17 12:06:37 +0530

    Added code for Lowest Common Ancestor of a Binary Search Tree

--- src/main/java/com/leetcode/solutions/tree/LowestCommonAncestor.java
+++ src/main/java/com/leetcode/solutions/tree/LowestCommonAncestor.java
@@ -9,13 +9,17 @@ package com.leetcode.solutions.tree;
 class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
+        // If value of P and Q is less that the root, got to left
         if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
 
+        // If value of P and Q is greater that the root, got to right
         if (p.val > root.val && q.val > root.val) {
             return lowestCommonAncestor(root.right, p, q);
         }
+
+        // If none condition is true, e.g if P is less that the root, and Q is greater than root, return root
         return root;
     }
 }
