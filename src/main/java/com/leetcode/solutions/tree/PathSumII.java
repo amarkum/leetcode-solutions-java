e6eed5e1015632a913459d5d79175bef992274a4
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-17 19:50:58 +0530

    Fixed Some formatting

--- src/main/java/com/leetcode/solutions/tree/PathSumII.java
+++ src/main/java/com/leetcode/solutions/tree/PathSumII.java
@@ -2,37 +2,40 @@ package com.leetcode.solutions.tree;
 
 /**
  * 437. Path Sum III - https://leetcode.com/problems/path-sum-iii/
- *
+ * <p>
  * You are given a binary tree in which each node contains an integer value.
  * Find the number of paths that sum to a given value.
  */
 
 class PathSumII {
     int totalPath = 0;
+
     public int pathSum(TreeNode root, int sum) {
-        if(root==null){
+        if (root == null) {
             return totalPath;
         }
         // Call the find path method, send 0 and if the value is returned equal to sum, increment totalPath
-        findPath(root,sum,0);
+        findPath(root, sum, 0);
 
         // Check each and every node recursively, and call the same method to find the path
         pathSum(root.left, sum);
-        pathSum(root.right,sum);
+        pathSum(root.right, sum);
 
         return totalPath;
 
     }
-    public void findPath(TreeNode node,int sum,int currentSum){
 
-        if(node==null){return;}
+    public void findPath(TreeNode node, int sum, int currentSum) {
+        if (node == null) {
+            return;
+        }
         currentSum += node.val;
-        if(currentSum == sum){
+        if (currentSum == sum) {
             totalPath++;
         }
 
-        findPath(node.left, sum,currentSum );
-        findPath(node.right,sum, currentSum);
+        findPath(node.left, sum, currentSum);
+        findPath(node.right, sum, currentSum);
     }
 
 }
