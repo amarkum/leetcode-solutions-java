17b5c39d81668b7a0d9f2ae81f25fe04c38888fe
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-18 18:16:54 +0530

    Updated javadoc for  Create Target Array in the Given Order

--- src/main/java/com/leetcode/solutions/arrays/TargetArray.java
+++ src/main/java/com/leetcode/solutions/arrays/TargetArray.java
@@ -7,6 +7,9 @@ import java.util.List;
  * 1389. Create Target Array in the Given Order - https://leetcode.com/problems/create-target-array-in-the-given-order/
  *
  * Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+ * Initially target array is empty.
+ * From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+ * Repeat the previous step until there are no elements to read in nums and index.
  */
 class TargetArray {
     public int[] createTargetArray(int[] nums, int[] index) {
