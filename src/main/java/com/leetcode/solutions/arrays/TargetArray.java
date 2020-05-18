099e57462f9def0673d0ef79ca8b023be567acbc
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-18 18:16:32 +0530

    Updated javadoc for  Create Target Array in the Given Order

--- src/main/java/com/leetcode/solutions/arrays/TargetArray.java
+++ src/main/java/com/leetcode/solutions/arrays/TargetArray.java
@@ -3,6 +3,11 @@ package com.leetcode.solutions.arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 1389. Create Target Array in the Given Order - https://leetcode.com/problems/create-target-array-in-the-given-order/
+ *
+ * Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+ */
 class TargetArray {
     public int[] createTargetArray(int[] nums, int[] index) {
         List<Integer> list = new ArrayList<Integer>();
