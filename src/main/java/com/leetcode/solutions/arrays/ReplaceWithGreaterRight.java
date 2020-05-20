488bd73ff184d26f5f0d1477a2293f593d69a5e8
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-20 17:29:32 +0530

    Added code for Replace Elements with Greatest Element on Right Side

---
+++ src/main/java/com/leetcode/solutions/arrays/ReplaceWithGreaterRight.java
@@ -0,0 +1,27 @@
+package com.leetcode.solutions.arrays;
+
+/**
+ * 1299. Replace Elements with Greatest Element on Right Side - https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+ * <p>
+ * Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise.
+ * Return the number of negative numbers in grid.
+ */
+class ReplaceWithGreaterRight {
+    public int[] replaceElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = maxValue(arr, i + 1);
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+
+    public int maxValue(int arr[], int start) {
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
