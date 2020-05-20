b2c0fad74658e4e829069699b4ba3225ad846990
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-21 01:23:50 +0530

    Added code for Reverse Words in a String III

---
+++ src/main/java/com/leetcode/solutions/strings/ReverseWordsInString.java
@@ -0,0 +1,20 @@
+package com.leetcode.solutions.strings;
+
+/**
+ * 557. Reverse Words in a String III - https://leetcode.com/problems/reverse-words-in-a-string-iii/
+ * <p>
+ * Given a string, you need to reverse the order of characters in each word within a sentence,
+ * while still preserving whitespace and initial word order.
+ */
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        String[] words = s.split(" ");
+
+        for (String word : words) {
+            sb.append(new StringBuilder(word).reverse().append(" "));
+        }
+        return sb.toString().substring(0, sb.length() - 1);
+    }
+}
