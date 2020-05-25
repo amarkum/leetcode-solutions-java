6bb0b27271b147e36fa2c4a46685578551977d7d
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-25 15:33:54 +0530

    Added code for Generate a String With Characters That Have Odd Counts

---
+++ src/main/java/com/leetcode/solutions/strings/OddCharacters.java
@@ -0,0 +1,29 @@
+package com.leetcode.solutions.strings;
+
+/**
+ * 1374. Generate a String With Characters That Have Odd Counts - https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+ * <p>
+ * Given an integer n,
+ * return a string with n characters such that each character in such string occurs an odd number of times.
+ * The returned string must contain only lowercase English letters. If there are multiples valid strings,
+ * return any of them.
+ */
+class OddCharacters {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        char a = 'p';
+        char b = 'z';
+        if (n % 2 == 0) {
+            for (int i = 0; i < n - 1; i++) {
+                sb.append(a);
+            }
+            sb.append(b);
+        } else {
+            for (int i = 0; i < n; i++) {
+                sb.append(a);
+            }
+        }
+        return sb.toString();
+    }
+}
